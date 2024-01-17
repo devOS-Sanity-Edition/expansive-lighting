37caf080340d90cde3789047579ec7529f20dce0
Stereo528 <52116771+Stereo528@users.noreply.github.com>
2024-01-16 19:59:14 -0700

    fixing torch order + adding textures

--- src/main/java/one/devos/nautical/expansive_lighting/ExpansiveLightingClient.java
+++ src/main/java/one/devos/nautical/expansive_lighting/ExpansiveLightingClient.java
@@ -1,11 +1,44 @@
 package one.devos.nautical.expansive_lighting;
 
+import net.minecraft.client.renderer.RenderType;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
+import org.quiltmc.qsl.block.extensions.api.client.BlockRenderLayerMap;
 
 public class ExpansiveLightingClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient(ModContainer mod) {
-
+		BlockRenderLayerMap.put(RenderType.cutout(), Registrar.WHITE_TORCH);
+		BlockRenderLayerMap.put(RenderType.cutout(), Registrar.LIGHT_GRAY_TORCH);
+		BlockRenderLayerMap.put(RenderType.cutout(), Registrar.GRAY_TORCH);
+		BlockRenderLayerMap.put(RenderType.cutout(), Registrar.BLACK_TORCH);
+		BlockRenderLayerMap.put(RenderType.cutout(), Registrar.BROWN_TORCH);
+		BlockRenderLayerMap.put(RenderType.cutout(), Registrar.RED_TORCH);
+		BlockRenderLayerMap.put(RenderType.cutout(), Registrar.ORANGE_TORCH);
+		BlockRenderLayerMap.put(RenderType.cutout(), Registrar.YELLOW_TORCH);
+		BlockRenderLayerMap.put(RenderType.cutout(), Registrar.LIME_TORCH);
+		BlockRenderLayerMap.put(RenderType.cutout(), Registrar.GREEN_TORCH);
+		BlockRenderLayerMap.put(RenderType.cutout(), Registrar.CYAN_TORCH);
+		BlockRenderLayerMap.put(RenderType.cutout(), Registrar.LIGHT_BLUE_TORCH);
+		BlockRenderLayerMap.put(RenderType.cutout(), Registrar.BLUE_TORCH);
+		BlockRenderLayerMap.put(RenderType.cutout(), Registrar.PURPLE_TORCH);
+		BlockRenderLayerMap.put(RenderType.cutout(), Registrar.MAGENTA_TORCH);
+		BlockRenderLayerMap.put(RenderType.cutout(), Registrar.PINK_TORCH);
+		BlockRenderLayerMap.put(RenderType.cutout(), Registrar.WHITE_WALL_TORCH);
+		BlockRenderLayerMap.put(RenderType.cutout(), Registrar.LIGHT_GRAY_WALL_TORCH);
+		BlockRenderLayerMap.put(RenderType.cutout(), Registrar.GRAY_WALL_TORCH);
+		BlockRenderLayerMap.put(RenderType.cutout(), Registrar.BLACK_WALL_TORCH);
+		BlockRenderLayerMap.put(RenderType.cutout(), Registrar.BROWN_WALL_TORCH);
+		BlockRenderLayerMap.put(RenderType.cutout(), Registrar.RED_WALL_TORCH);
+		BlockRenderLayerMap.put(RenderType.cutout(), Registrar.ORANGE_WALL_TORCH);
+		BlockRenderLayerMap.put(RenderType.cutout(), Registrar.YELLOW_WALL_TORCH);
+		BlockRenderLayerMap.put(RenderType.cutout(), Registrar.LIME_WALL_TORCH);
+		BlockRenderLayerMap.put(RenderType.cutout(), Registrar.GREEN_WALL_TORCH);
+		BlockRenderLayerMap.put(RenderType.cutout(), Registrar.CYAN_WALL_TORCH);
+		BlockRenderLayerMap.put(RenderType.cutout(), Registrar.LIGHT_BLUE_WALL_TORCH);
+		BlockRenderLayerMap.put(RenderType.cutout(), Registrar.BLUE_WALL_TORCH);
+		BlockRenderLayerMap.put(RenderType.cutout(), Registrar.PURPLE_WALL_TORCH);
+		BlockRenderLayerMap.put(RenderType.cutout(), Registrar.MAGENTA_WALL_TORCH);
+		BlockRenderLayerMap.put(RenderType.cutout(), Registrar.PINK_WALL_TORCH);
 	}
 }
--- src/main/java/one/devos/nautical/expansive_lighting/Registrar.java
+++ src/main/java/one/devos/nautical/expansive_lighting/Registrar.java
@@ -23,57 +23,58 @@ public class Registrar {
 	public static final Block VARIABLE_REDSTONE_LAMP = registerBlock("variable_redstone_lamp", new VariableRedstoneLampBlock(BlockBehaviour.Properties.of().strength(0.3F).sound(SoundType.GLASS).isValidSpawn(Blocks::always).lightLevel(VariableRedstoneLampBlock.LIGHT_EMISSION)));
 
 	//Torches
+	public static final Block WHITE_TORCH = registerBlockNoItem("white_torch", new TorchBlock(BlockBehaviour.Properties.copy(TORCH), ParticleTypes.FLAME));
+	public static final Block LIGHT_GRAY_TORCH = registerBlockNoItem("light_gray_torch", new TorchBlock(BlockBehaviour.Properties.copy(TORCH), ParticleTypes.FLAME));
+	public static final Block GRAY_TORCH = registerBlockNoItem("gray_torch", new TorchBlock(BlockBehaviour.Properties.copy(TORCH), ParticleTypes.FLAME));
+	public static final Block BLACK_TORCH = registerBlockNoItem("black_torch", new TorchBlock(BlockBehaviour.Properties.copy(TORCH), ParticleTypes.FLAME));
+	public static final Block BROWN_TORCH = registerBlockNoItem("brown_torch", new TorchBlock(BlockBehaviour.Properties.copy(TORCH), ParticleTypes.FLAME));
 	public static final Block RED_TORCH = registerBlockNoItem("red_torch", new TorchBlock(BlockBehaviour.Properties.copy(TORCH), ParticleTypes.FLAME));
 	public static final Block ORANGE_TORCH = registerBlockNoItem("orange_torch", new TorchBlock(BlockBehaviour.Properties.copy(TORCH), ParticleTypes.FLAME));
 	public static final Block YELLOW_TORCH = registerBlockNoItem("yellow_torch", new TorchBlock(BlockBehaviour.Properties.copy(TORCH), ParticleTypes.FLAME));
+	public static final Block LIME_TORCH = registerBlockNoItem("lime_torch", new TorchBlock(BlockBehaviour.Properties.copy(TORCH), ParticleTypes.FLAME));
 	public static final Block GREEN_TORCH = registerBlockNoItem("green_torch", new TorchBlock(BlockBehaviour.Properties.copy(TORCH), ParticleTypes.FLAME));
-	public static final Block BLUE_TORCH = registerBlockNoItem("blue_torch", new TorchBlock(BlockBehaviour.Properties.copy(TORCH), ParticleTypes.FLAME));
 	public static final Block CYAN_TORCH = registerBlockNoItem("cyan_torch", new TorchBlock(BlockBehaviour.Properties.copy(TORCH), ParticleTypes.FLAME));
+	public static final Block LIGHT_BLUE_TORCH = registerBlockNoItem("light_blue_torch", new TorchBlock(BlockBehaviour.Properties.copy(TORCH), ParticleTypes.FLAME));
+	public static final Block BLUE_TORCH = registerBlockNoItem("blue_torch", new TorchBlock(BlockBehaviour.Properties.copy(TORCH), ParticleTypes.FLAME));
+	public static final Block PURPLE_TORCH = registerBlockNoItem("purple_torch", new TorchBlock(BlockBehaviour.Properties.copy(TORCH), ParticleTypes.FLAME));
 	public static final Block MAGENTA_TORCH = registerBlockNoItem("magenta_torch", new TorchBlock(BlockBehaviour.Properties.copy(TORCH), ParticleTypes.FLAME));
 	public static final Block PINK_TORCH = registerBlockNoItem("pink_torch", new TorchBlock(BlockBehaviour.Properties.copy(TORCH), ParticleTypes.FLAME));
-	public static final Block PURPLE_TORCH = registerBlockNoItem("purple_torch", new TorchBlock(BlockBehaviour.Properties.copy(TORCH), ParticleTypes.FLAME));
-	public static final Block LIME_TORCH = registerBlockNoItem("lime_torch", new TorchBlock(BlockBehaviour.Properties.copy(TORCH), ParticleTypes.FLAME));
-	public static final Block LIGHT_BLUE_TORCH = registerBlockNoItem("light_blue_torch", new TorchBlock(BlockBehaviour.Properties.copy(TORCH), ParticleTypes.FLAME));
-	public static final Block BROWN_TORCH = registerBlockNoItem("brown_torch", new TorchBlock(BlockBehaviour.Properties.copy(TORCH), ParticleTypes.FLAME));
-	public static final Block WHITE_TORCH = registerBlockNoItem("white_torch", new TorchBlock(BlockBehaviour.Properties.copy(TORCH), ParticleTypes.FLAME));
-	public static final Block BLACK_TORCH = registerBlockNoItem("black_torch", new TorchBlock(BlockBehaviour.Properties.copy(TORCH), ParticleTypes.FLAME));
-	public static final Block GRAY_TORCH = registerBlockNoItem("gray_torch", new TorchBlock(BlockBehaviour.Properties.copy(TORCH), ParticleTypes.FLAME));
-	public static final Block LIGHT_GRAY_TORCH = registerBlockNoItem("light_gray_torch", new TorchBlock(BlockBehaviour.Properties.copy(TORCH), ParticleTypes.FLAME));
+
 	//Wall Torches
+	public static final Block WHITE_WALL_TORCH = registerBlockNoItem("white_wall_torch", new WallTorchBlock(BlockBehaviour.Properties.copy(WALL_TORCH), ParticleTypes.FLAME));
+	public static final Block LIGHT_GRAY_WALL_TORCH = registerBlockNoItem("light_gray_wall_torch", new WallTorchBlock(BlockBehaviour.Properties.copy(WALL_TORCH), ParticleTypes.FLAME));
+	public static final Block GRAY_WALL_TORCH = registerBlockNoItem("gray_wall_torch", new WallTorchBlock(BlockBehaviour.Properties.copy(WALL_TORCH), ParticleTypes.FLAME));
+	public static final Block BLACK_WALL_TORCH = registerBlockNoItem("black_wall_torch", new WallTorchBlock(BlockBehaviour.Properties.copy(WALL_TORCH), ParticleTypes.FLAME));
+	public static final Block BROWN_WALL_TORCH = registerBlockNoItem("brown_wall_torch", new WallTorchBlock(BlockBehaviour.Properties.copy(WALL_TORCH), ParticleTypes.FLAME));
 	public static final Block RED_WALL_TORCH = registerBlockNoItem("red_wall_torch", new WallTorchBlock(BlockBehaviour.Properties.copy(WALL_TORCH), ParticleTypes.FLAME));
 	public static final Block ORANGE_WALL_TORCH = registerBlockNoItem("orange_wall_torch", new WallTorchBlock(BlockBehaviour.Properties.copy(WALL_TORCH), ParticleTypes.FLAME));
 	public static final Block YELLOW_WALL_TORCH = registerBlockNoItem("yellow_wall_torch", new WallTorchBlock(BlockBehaviour.Properties.copy(WALL_TORCH), ParticleTypes.FLAME));
+	public static final Block LIME_WALL_TORCH = registerBlockNoItem("lime_wall_torch", new WallTorchBlock(BlockBehaviour.Properties.copy(WALL_TORCH), ParticleTypes.FLAME));
 	public static final Block GREEN_WALL_TORCH = registerBlockNoItem("green_wall_torch", new WallTorchBlock(BlockBehaviour.Properties.copy(WALL_TORCH), ParticleTypes.FLAME));
-	public static final Block BLUE_WALL_TORCH = registerBlockNoItem("blue_wall_torch", new WallTorchBlock(BlockBehaviour.Properties.copy(WALL_TORCH), ParticleTypes.FLAME));
+	public static final Block LIGHT_BLUE_WALL_TORCH = registerBlockNoItem("light_blue_wall_torch", new WallTorchBlock(BlockBehaviour.Properties.copy(WALL_TORCH), ParticleTypes.FLAME));
 	public static final Block CYAN_WALL_TORCH = registerBlockNoItem("cyan_wall_torch", new WallTorchBlock(BlockBehaviour.Properties.copy(WALL_TORCH), ParticleTypes.FLAME));
+	public static final Block BLUE_WALL_TORCH = registerBlockNoItem("blue_wall_torch", new WallTorchBlock(BlockBehaviour.Properties.copy(WALL_TORCH), ParticleTypes.FLAME));
+	public static final Block PURPLE_WALL_TORCH = registerBlockNoItem("purple_wall_torch", new WallTorchBlock(BlockBehaviour.Properties.copy(WALL_TORCH), ParticleTypes.FLAME));
 	public static final Block MAGENTA_WALL_TORCH = registerBlockNoItem("magenta_wall_torch", new WallTorchBlock(BlockBehaviour.Properties.copy(WALL_TORCH), ParticleTypes.FLAME));
 	public static final Block PINK_WALL_TORCH = registerBlockNoItem("pink_wall_torch", new WallTorchBlock(BlockBehaviour.Properties.copy(WALL_TORCH), ParticleTypes.FLAME));
-	public static final Block PURPLE_WALL_TORCH = registerBlockNoItem("purple_wall_torch", new WallTorchBlock(BlockBehaviour.Properties.copy(WALL_TORCH), ParticleTypes.FLAME));
-	public static final Block LIME_WALL_TORCH = registerBlockNoItem("lime_wall_torch", new WallTorchBlock(BlockBehaviour.Properties.copy(WALL_TORCH), ParticleTypes.FLAME));
-	public static final Block LIGHT_BLUE_WALL_TORCH = registerBlockNoItem("light_blue_wall_torch", new WallTorchBlock(BlockBehaviour.Properties.copy(WALL_TORCH), ParticleTypes.FLAME));
-	public static final Block BROWN_WALL_TORCH = registerBlockNoItem("brown_wall_torch", new WallTorchBlock(BlockBehaviour.Properties.copy(WALL_TORCH), ParticleTypes.FLAME));
-	public static final Block WHITE_WALL_TORCH = registerBlockNoItem("white_wall_torch", new WallTorchBlock(BlockBehaviour.Properties.copy(WALL_TORCH), ParticleTypes.FLAME));
-	public static final Block BLACK_WALL_TORCH = registerBlockNoItem("black_wall_torch", new WallTorchBlock(BlockBehaviour.Properties.copy(WALL_TORCH), ParticleTypes.FLAME));
-	public static final Block GRAY_WALL_TORCH = registerBlockNoItem("gray_wall_torch", new WallTorchBlock(BlockBehaviour.Properties.copy(WALL_TORCH), ParticleTypes.FLAME));
-	public static final Block LIGHT_GRAY_WALL_TORCH = registerBlockNoItem("light_gray_wall_torch", new WallTorchBlock(BlockBehaviour.Properties.copy(WALL_TORCH), ParticleTypes.FLAME));
 
 	//God I hate torches so fucking much
+	public static final Item WHITE_TORCH_ITEM = registerBlockItem((BlockItem) new StandingAndWallBlockItem(WHITE_TORCH, WHITE_WALL_TORCH, new Item.Properties(), Direction.DOWN), WHITE_TORCH);
+	public static final Item LIGHT_GRAY_TORCH_ITEM = registerBlockItem((BlockItem) new StandingAndWallBlockItem(LIGHT_GRAY_TORCH, LIGHT_GRAY_WALL_TORCH, new Item.Properties(), Direction.DOWN), LIGHT_GRAY_TORCH);
+	public static final Item GRAY_TORCH_ITEM = registerBlockItem((BlockItem) new StandingAndWallBlockItem(GRAY_TORCH, GRAY_WALL_TORCH, new Item.Properties(), Direction.DOWN), GRAY_TORCH);
+	public static final Item BLACK_TORCH_ITEM = registerBlockItem((BlockItem) new StandingAndWallBlockItem(BLACK_TORCH, BLACK_WALL_TORCH, new Item.Properties(), Direction.DOWN), BLACK_TORCH);
+	public static final Item BROWN_TORCH_ITEM = registerBlockItem((BlockItem) new StandingAndWallBlockItem(BROWN_TORCH, BROWN_WALL_TORCH, new Item.Properties(), Direction.DOWN), BROWN_TORCH);
 	public static final Item RED_TORCH_ITEM = registerBlockItem((BlockItem) new StandingAndWallBlockItem(RED_TORCH, RED_WALL_TORCH, new Item.Properties(), Direction.DOWN), RED_TORCH);
 	public static final Item ORANGE_TORCH_ITEM = registerBlockItem((BlockItem) new StandingAndWallBlockItem(ORANGE_TORCH, ORANGE_WALL_TORCH, new Item.Properties(), Direction.DOWN), ORANGE_TORCH);
 	public static final Item YELLOW_TORCH_ITEM = registerBlockItem((BlockItem) new StandingAndWallBlockItem(YELLOW_TORCH, YELLOW_WALL_TORCH, new Item.Properties(), Direction.DOWN), YELLOW_TORCH);
+	public static final Item LIME_TORCH_ITEM = registerBlockItem((BlockItem) new StandingAndWallBlockItem(LIME_TORCH, LIME_WALL_TORCH, new Item.Properties(), Direction.DOWN), LIME_TORCH);
 	public static final Item GREEN_TORCH_ITEM = registerBlockItem((BlockItem) new StandingAndWallBlockItem(GREEN_TORCH, GREEN_WALL_TORCH, new Item.Properties(), Direction.DOWN), GREEN_TORCH);
-	public static final Item BLUE_TORCH_ITEM = registerBlockItem((BlockItem) new StandingAndWallBlockItem(BLUE_TORCH, BLUE_WALL_TORCH, new Item.Properties(), Direction.DOWN), BLUE_TORCH);
 	public static final Item CYAN_TORCH_ITEM = registerBlockItem((BlockItem) new StandingAndWallBlockItem(CYAN_TORCH, CYAN_WALL_TORCH, new Item.Properties(), Direction.DOWN), CYAN_TORCH);
+	public static final Item LIGHT_BLUE_TORCH_ITEM = registerBlockItem((BlockItem) new StandingAndWallBlockItem(LIGHT_BLUE_TORCH, LIGHT_BLUE_WALL_TORCH, new Item.Properties(), Direction.DOWN), LIGHT_BLUE_TORCH);
+	public static final Item BLUE_TORCH_ITEM = registerBlockItem((BlockItem) new StandingAndWallBlockItem(BLUE_TORCH, BLUE_WALL_TORCH, new Item.Properties(), Direction.DOWN), BLUE_TORCH);
+	public static final Item PURPLE_TORCH_ITEM = registerBlockItem((BlockItem) new StandingAndWallBlockItem(PURPLE_TORCH, PURPLE_WALL_TORCH, new Item.Properties(), Direction.DOWN), PURPLE_TORCH);
 	public static final Item MAGENTA_TORCH_ITEM = registerBlockItem((BlockItem) new StandingAndWallBlockItem(MAGENTA_TORCH, MAGENTA_WALL_TORCH, new Item.Properties(), Direction.DOWN), MAGENTA_TORCH);
 	public static final Item PINK_TORCH_ITEM = registerBlockItem((BlockItem) new StandingAndWallBlockItem(PINK_TORCH, PINK_WALL_TORCH, new Item.Properties(), Direction.DOWN), PINK_TORCH);
-	public static final Item PURPLE_TORCH_ITEM = registerBlockItem((BlockItem) new StandingAndWallBlockItem(PURPLE_TORCH, PURPLE_WALL_TORCH, new Item.Properties(), Direction.DOWN), PURPLE_TORCH);
-	public static final Item LIME_TORCH_ITEM = registerBlockItem((BlockItem) new StandingAndWallBlockItem(LIME_TORCH, LIME_WALL_TORCH, new Item.Properties(), Direction.DOWN), LIME_TORCH);
-	public static final Item LIGHT_BLUE_TORCH_ITEM = registerBlockItem((BlockItem) new StandingAndWallBlockItem(LIGHT_BLUE_TORCH, LIGHT_BLUE_WALL_TORCH, new Item.Properties(), Direction.DOWN), LIGHT_BLUE_TORCH);
-	public static final Item BROWN_TORCH_ITEM = registerBlockItem((BlockItem) new StandingAndWallBlockItem(BROWN_TORCH, BROWN_WALL_TORCH, new Item.Properties(), Direction.DOWN), BROWN_TORCH);
-	public static final Item WHITE_TORCH_ITEM = registerBlockItem((BlockItem) new StandingAndWallBlockItem(WHITE_TORCH, WHITE_WALL_TORCH, new Item.Properties(), Direction.DOWN), WHITE_TORCH);
-	public static final Item BLACK_TORCH_ITEM = registerBlockItem((BlockItem) new StandingAndWallBlockItem(BLACK_TORCH, BLACK_WALL_TORCH, new Item.Properties(), Direction.DOWN), BLACK_TORCH);
-	public static final Item GRAY_TORCH_ITEM = registerBlockItem((BlockItem) new StandingAndWallBlockItem(GRAY_TORCH, GRAY_WALL_TORCH, new Item.Properties(), Direction.DOWN), GRAY_TORCH);
-	public static final Item LIGHT_GRAY_TORCH_ITEM = registerBlockItem((BlockItem) new StandingAndWallBlockItem(LIGHT_GRAY_TORCH, LIGHT_GRAY_WALL_TORCH, new Item.Properties(), Direction.DOWN), LIGHT_GRAY_TORCH);
 
 	public static Block registerBlock(String name, Block block) {
 		BlockItem item = new BlockItem(block, new QuiltItemSettings());
